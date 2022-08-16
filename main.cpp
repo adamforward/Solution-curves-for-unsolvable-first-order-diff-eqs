6fed933cb71518a59201729143942eac9450d51e
Adam Forward <111149293+adamforward@users.noreply.github.com>
2022-08-16 04:36:48 +0000

    got it to work

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
-
 #include <iostream>
 #include <string> 
 #include <stack> 
 #include <vector> 
-#include <queue>
 #include <math.h>
 #include <limits.h> 
 #include <string.h> 
+#include <sstream>
 using namespace std; 
+
 bool isNumber(string n){
   bool z; 
  for (int i=0;i<n.length();i++) {
-   if(n=="0"||n=="1"||n=="2"||n=="3"||n=="4"||n=="5"||n=="6"||n=="7"||n=="8"||n =="9")//y^t get replaced by numbers when the string is evaluated. 
+   if(n[i]=='0'||n[i]=='1'||n[i]=='2'||n[i]=='3'||n[i]=='4'||n[i]=='5'||n[i]=='6'||n[i]=='7'||n[i]=='8'||n[i] =='9')//y^t get replaced by numbers when the string is evaluated. 
    z=true; 
   else 
    z=false;
@@ -22,8 +22,10 @@ bool isNumber(string n){
 bool isOperand(string n){ 
   bool z; 
  for (int i=0;i<n.length();i++) {
-   if(n=="0"||n=="1"||n=="2"||n=="3"||n=="4"||n=="5"||n=="6"||n=="7"||n=="8"||n =="9"||n=="y"||n=="t"||n=="e"||n=="pi")//y^t get replaced by numbers when the string is evaluated. 
+   if(n=="y"||n=="t"||n=="e"||n=="pi")//y^t get replaced by numbers when the string is evaluated. 
    z=true; 
+  else if(isNumber(n)==true)
+    z=true;
   else 
    return false;
   //test all functions before using. this one works. 
@@ -84,66 +86,80 @@ bool isOp(string s){
   else
     return false; 
 }
-stack<string> infixToPostfix(vector<string> infix){//this is the repl it where I am going to get this function working. 
+vector<string> infixToPostfix(vector<string> infix){
 stack<string> ops; 
-  stack<string> res; 
+  vector<string> res; 
   
   //infix to postfix 
   for (int i=0; i<infix.size(); i++){
+    string c=infix[i]; 
     //1. Print operands as they arrive.
-    if (isOperand(infix[i])==true){
-      res.push(infix[i]); 
-      continue; 
+    if (isOperand(c)==true){
+      res.push_back(c);   
       }
-    //2. If the stack is empty or contains a left parenthesis on top, push the incoming operator onto the stack.
-    if(isOp(infix[i])==true&&(ops.empty()==true|| isLeftParenthases(ops.top())==true)){
-      ops.push(infix[i]);
-      continue;
-      }
-    //3. If the incoming symbol is a left parenthesis, push it on the stack.
-    if(isLeftParenthases(infix[i])) {
-      ops.push(infix[i]);
-      continue; 
+      //3. If the incoming symbol is a left parenthesis, push it on the stack.
+    else if(isLeftParenthases(c)) {
+      ops.push(c); 
       }
       // 4. If the incoming symbol is a right parenthesis, pop the stack and print the operators until you see a left parenthesis. Discard the pair of parentheses.
-    if(isRightParenthases(infix[i])==true){
+     else if(isRightParenthases(infix[i])==true){
       while(ops.top()!="("){
-        res.push(ops.top()); 
+        res.push_back(ops.top()); 
         ops.pop(); 
       }
       ops.pop();
-       //get rid of left paren. 
+       //get rid of left paren.  
         }
-      //5. If the incoming symbol has higher precedence than the top of the stack, push it on the stack.
-      if (isOp(infix[i])&&priority(ops.top())<priority(infix[i]))
-        ops.push(infix[i]);
+    //2. If the stack is empty or contains a left parenthesis on top, push the incoming operator onto the stack.
+      else if(ops.empty()==true&&isOp(c)==true){
+      ops.push(c);
+      continue;
+      }
+    else if(isOp(c)==true&&ops.empty()==false){
+     if(isLeftParenthases(ops.top())==true){
+      ops.push(c); 
       continue; 
-    //6. If the incoming symbol has equal precedence with the top of the stack, use association. If the association is left to right, pop and print the top of the stack and then push the incoming operator. If the association is right to left, push the incoming operator. so A + B - C sin(tan(x))=xtansin-doesn't matter for prefix expressions due to parens so this rule is only for ops A + B - C prio(+)==prio(-), you want AB+C- 
-      if(isOperator(infix[i])==true&&priority(infix[i])==priority(ops.top())){
-          res.push(ops.top()); 
-          ops.pop(); 
-          ops.push(infix[i]);
-          continue; 
-        } 
+      }
       
-        //7. If the incoming symbol has lower precedence than the symbol on the top of the stack, pop the stack and print the top operator. Then test the incoming operator against the new top of stack.
-        if((isOperator(infix[i])==true||isPrefix(infix[i])==true)&& (priority(ops.top())>priority(infix[i]))){   
-            while(priority(ops.top())>priority(infix[i])||ops.empty()==true){
-          res.push(ops.top());
-          ops.pop();
+      //5. If the incoming symbol has higher precedence than the top of the stack, push it on the stack.
+     if (priority(ops.top())<priority(c)){
+        ops.push(c);
+       continue; 
+        }
+    //6. If the incoming symbol has equal precedence with the top of the stack, use association. If the association is left to right, pop and print the top of the stack and then push the incoming operator. If the association is right to left, push the incoming operator. so A + B - C sin(tan(x))=xtansin-doesn't matter for prefix expressions due to parens so this rule is only for ops A + B - C prio(+)==prio(-), you want AB+C- 
+     else if(priority(c)==priority(ops.top())){
+          string b=ops.top(); 
+          res.push_back(b); 
+          ops.pop(); 
+          ops.push(infix[i]);       
+       }
+        //7. If the incoming symbol has lower priority than the symbol on the top of the stack, pop the stack and print the top operator. Then test the incoming operator against the new top of stack.
+      else if(priority(ops.top())>priority(c)){   
+            while(priority(ops.top())>priority(c)){
+            string a=ops.top();
+            res.push_back(a); 
+            ops.pop();
+            if(ops.empty()==true)
+              break; 
           }
           ops.push(infix[i]); 
-          
         }
-        }//8. At the end of the expression, pop and print all operators on the stack. for loop ends here
+      }
+    else{
+      res.push_back("not valid");
+    }
+    
+        }//exit for loop 
+       //8. At the end of the expression, pop and print all operators on the stack. for loop ends here
     while(ops.empty()!=true){
-      res.push(ops.top());
+      res.push_back(ops.top());
       ops.pop();
     }
     //next, test this algorithm. then write out the evaluate function and do the euler stuff in the main. 
     return res;
 //return res; 
         }
+
 float oneNumOp(float a, string op){
       if(op=="sin")
         return sin(a);
@@ -188,6 +204,7 @@ float twoNumOp(float a, float b, string op){
       else
    return INT_MIN; //return negative infinity
 }
+
 /*
 Start reading the expression from left to right.
 If the element is an operand then, push it in the stack.
@@ -197,19 +214,11 @@ Keep repeating the above steps until the end of the expression isreached.
 The final result will be now left in the stack, display the same.
 convert the stack over to a vector of strings first, loop through it backwards 
 */
-vector<string> stackToVector(stack<string> stack){ 
-  vector<string> a;
-  while(stack.empty()==false){
-    a.push_back(stack.top()); 
-    stack.pop(); 
-  }
-  return a; 
-}
 
 float betterEvalPostFix(vector<string> postfix, float y, float t){
   stack<float> answer;
   float conv=0; 
-  for(int i=postfix.size()-1; i>=0; i--){//Start reading the expression from left to right.
+  for(int i=0; i<postfix.size(); i++){//Start reading the expression from left to right.
     if(isOperator(postfix[i])==true && answer.size()<2)
       return 340282346638528859811704183484516925440.0000000000000000;
     if (isPrefix(postfix[i])==true && answer.empty()==true)
@@ -246,19 +255,23 @@ float betterEvalPostFix(vector<string> postfix, float y, float t){
   else 
     return 340282346638528859811704183484516925440.0000000000000000;
 }
+
   void error(){
     cout<<"Error: please enter a valid infix expression"<<endl; 
     return; 
   }
-    
-    float euler(vector<string> postfix, float y, float t, float stepsize){
-        float ynext=y+stepsize*betterEvalPostFix(postfix, y, t); 
-        return ynext; 
+    /*
+    float euler(vector<string> postfix, float curry, float currt, float stepsize){
+        float yprime=betterEvalPostFix(postfix, curry, currt);
+        float yNext=curry+yprime*stepsize; 
+        return yNext; 
       }
+  */
 /*
   vector<string> splitString(string infix){
     vector<string> inf; 
-    int currIndex = 0, i = 0;  
+    int currIndex = 0;
+    int i=0; 
     int startIndex = 0, endIndex = 0;  
     while (i <= infix.size())  
     {  
@@ -272,33 +285,75 @@ float betterEvalPostFix(vector<string> postfix, float y, float t){
             startIndex = endIndex + 1;  
         }  
         i++;  
-        }
+        } 
   }
 */
-
     void errorTwo(){
       cout<<"please enter a step size greater than zero"<<endl; 
       return; 
     }
     void errorThree(){
       cout<<"please enter a t final value greater than tzero"<<endl; 
+
     }
+
+/*
+char *token;
+char *line = "LINE TO BE SEPARATED";
+char *search = " ";
+Token will point to "LINE". 
+token = strtok(line, search);
+
+*/
+/*
+  vector<string> splitStringTwo(string infix){
+    char inf[infix.size()]; 
+    vector<string> answer;
+    for (int i=0; i<infix.size(); i++){
+      inf[i]=infix[i]; 
+    }
+  char * pointer;
+  pointer = strtok (inf,"|");
+  string a=
+  answer.push_back(a); 
+  while (pointer != NULL)
+  {
+    pointer = strtok (NULL, "|");
+    answer.push_back(a); 
+  }
+    }
+  
+*/
+//after these two attempts at the splitstring algorithm, I did some more research and found out how to use the stringstream data type
+
 int main() {
+  
   //all the code from here
   cout<<"Adam Forward"<<endl; 
   cout<<"  Euler's method can be used to approximate the solution curve for any ordinary first order differential equation in the form dy/dt=f(y,t), where the solution is a function y(t)."<<endl; 
   cout<<"  While using methods such as integrating factors or seperation of variables to obtain a solution y(t) is ideal for analysing any given first order differential equation, many are unsolvable and require euler's method to approximate any given y value at some point t."<<endl; 
   cout<<"  Euler's method evaluates each point y(n+1) as y(n)+h*f(y,t) where f(y,t) is equal to dy/dt. However, this method can require an excessive number of calculations so programming is often a nessessary tool for analysing unsolvable first order differential equations. As the step size, h, decreases, the accurracy for which the solution curve represents the function, y(t), increases while the runtime of the program also increases."<<endl; 
-  cout<<"  This program uses the standard library of C++ and includes the iostream, string, vector, stack, queue and math.h libraries. Using the input of any string that properly represents an infix expression for the function dy/dt, this problem is able to produce a set of values y and t that represent different points along the solution curve up until any given t value. Initially, the input string is broken up into a vector of strings seperating the operands and operators. The arguements taken as operands in this program are: y, t, e, pi, and any string of numbers. The arguements taken as operators are (, ), ^, *, +, -, /, sin, cos, tan, arcsin, arccos, arctan and ln. The vector of strings is then converted into a stack of strings organized by a modified postfix notation where trig and logarithm functions have priority over every operator besides (. At each function call of the euler formula, this postfix expression is evaluated while taking new arguements for y and t each time. "<<endl; 
+  cout<<"  This program uses the standard library of C++ and includes the iostream, string, vector, stack, string.h, limits.h, sstream and math.h libraries. Using the input of any string that properly represents an infix expression for the function dy/dt, this problem is able to produce a set of values y and t that represent different points along the solution curve up until any given t value. Initially, the input string is broken up into a vector of strings seperating the operands and operators. The arguements taken as operands in this program are: y, t, e, pi, and any string of numbers. The arguements taken as operators are (, ), ^, *, +, -, /, sin, cos, tan, arcsin, arccos, arctan and ln. The vector of strings is then converted into a stack of strings organized by a modified postfix notation where trig and logarithm functions have priority over every operator besides (. At each function call of the euler formula, this postfix expression is evaluated while taking new arguements for y and t each time. "<<endl; 
   bool valid=false;
   vector<string> postFixVector; 
   while(valid==false){
+
+
   string infixString;
   cout<<"Enter a first order differential equation in the form dy/dt=f(y,t). Note: this has to be a valid infix expression"<<endl; 
   cin>>infixString; 
-  vector<string> infixVector=splitString(infixString); 
-  stack<string> postFixStack=infixToPostfix(infixVector);
-  postFixVector=stackToVector(postFixStack); 
+  string intermediate=""; 
+  stringstream ss(infixString);
+  vector <string> tokens; 
+  while(getline(ss, intermediate, '|')){//string splitting function works 
+    tokens.push_back(intermediate); 
+  }
+  postFixVector=infixToPostfix(tokens);
+  for(int i=0;i<postFixVector.size();i++){
+    if (postFixVector[i]=="not valid")
+      error();
+      continue; 
+    } 
     if(betterEvalPostFix(postFixVector, 5, 5)!=340282346638528859811704183484516925440.0000000000000000){
       valid=true; 
       }
@@ -316,7 +371,7 @@ int main() {
   while(validStepSize==false){
   cout<<"what is your stepsize"<<endl;
     cin>>stepSize; 
-    if (stepSize<0)
+    if (stepSize>0)
       validStepSize=true; 
     else 
       errorTwo(); 
@@ -331,22 +386,26 @@ int main() {
     else
       validTFinal=true; 
   }
-  vector<float> yVals;
-  vector<float> tVals; 
-  cout<<"here are the approximated values in your solution curve:"<<endl; 
-  cout<<"t="<<t<<" y(t)="<<y<<endl;
-  while(t<=tFinal){
-    y=euler(postFixVector, y, t, stepSize);
-    yVals.push_back(y); 
-    t+=stepSize; 
-    tVals.push_back(t); 
+  for(int i=0;i<postFixVector.size(); i++){
+    cout<<postFixVector[i]<<endl; 
   }
   
-  for(int i=0; i<tVals.size(); i++){
-    cout<<"t="<<t<<" y(t)="<<y<<endl;
+  vector<float> yVals;
+  vector<float> tVals; 
+  yVals.push_back(y); 
+  tVals.push_back(t);
+  cout<<"here are the approximated values in your solution curve:"<<endl; 
+  cout<<" yPrime="<<yPrime<<" t="<<t<<" y="<<y<<endl;
+  while(t<tFinal){
+    float yPrime=betterEvalPostFix(postFixVector,y,t);  
+    float yIncrease=yPrime*stepSize; 
+    t=t+stepSize; 
+    y=y+yIncrease; 
+    cout<<" yPrime="<<yPrime<<" t="<<t<<" y="<<y<<endl;
   }
-  return 0; 
+  
     //to here is part of the main that doesn't test functions
+
     /*
   string a; 
   vector<string> b; 
@@ -356,6 +415,7 @@ int main() {
     b.push_back(a);
 }
 */
+
   /*
   stack<string> c=infixToPostfix(b); 
   vector<float> yvals; 
@@ -381,7 +441,8 @@ int main() {
   for(int i=0; i<yvals.size(); i++){
     cout<<"t="<<tvals[i]<<"   y(t)="<<yvals[i]<<endl; 
   }
-  
+*/
+  /*
   vector<string> a; 
   a.push_back("+");
   a.push_back("4");
@@ -390,5 +451,14 @@ int main() {
   return 0; 
   */
   //tester for splitString(comment everything else to test)
+  /*
+  string b; 
+  cin>>b; 
+  vector<string> c=splitString(b); 
+  for(int i=0; i<c.size(); i++){
+    cout<<c[i]<<endl; 
+  }
+  */
   
+  return 0; 
   } 
